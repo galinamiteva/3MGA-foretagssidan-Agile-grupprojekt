4d2747b2dac0f97db9dc0d826333b340e2dcf49d
Arber Sefa <arber.sefa@utb.ecutbildning.se>
2021-09-07 14:31:22 +0200

    website update

--- news/news.js
+++ news/news.js
@@ -1,14 +1,3 @@
-/*vi ska göra dynamisk denna arkitektur
-        <section class="list">
-            <div class="list-item">   
-                <div class="item-name   items"> TITLE </div>
-                <div class="item-author  items"> AUTHOR </div>
-                <div class="item-meta items"> TYPE </div>
-                <div class="item-date  items">  Data - number </div>
-                
-            </div>
-        </section>
-*/
 
 
 const list = document.querySelector('.list');
@@ -18,24 +7,13 @@ const forms = document.forms;
 // const sort_name_btn =document.querySelector('.sort-options .sort-name');
 // const sort_author_btn = document.querySelector('.sort-options .sort-author') 
 const sort_date_btn = document.querySelector('.sort-options .sort-date')
-// const sort_meta_btn = document.querySelector('.sort-options .sort-meta')
 
 
-fetch("news.json")
+fetch("/news/news.json")
   .then(function (resp) {
     return resp.json();
   })
   .then(function (data) {
-    // console.log(data);
-
-
-
-
-
-
-
-
-
 
 
     for (let i = 0; i < data.length; i++) {
@@ -51,13 +29,7 @@ fetch("news.json")
 
       });
 
-      // sort_meta_btn.addEventListener('click', () => {
-      //   let array = sort_array_by(data, 'type', desc);
-      //   displayList(array);
 
-      //   desc = !desc;
-
-      // });
 
 
 
@@ -83,18 +55,12 @@ fetch("news.json")
     function displayList(array = []) {
       list.innerHTML = "";
 
-      // console.log(selectFitler.value);
-
-
-
-
-      // console.log('alla alla')
 
       for (let i = 0; i < array.length; i++) {
         let item = array[i];
         let item_element = document.createElement('div');
         item_element.classList.add('list-item');     // 2 namn på classen <div class="item-name   items"> TITLE </div>
-        // console.log(item.type)
+
 
         if (item.type == 'article') {
           item_element.classList.add('article-item');
@@ -142,26 +108,6 @@ fetch("news.json")
 
 
 
-
-
-      // } else if (selectFitler.value == 'artikel') {
-
-      //   console.log('article article')
-      // }
-
-      // else if (selectFitler.value == 'nyhter') {
-
-      //   console.log('nyhter nyhter')
-      // }
-
-
-
-
-
-
-
-
-      // console.log(item.type)
 
       const selectFitler = document.querySelector('#filter')
       const articleItems = document.querySelectorAll('.article-item')
@@ -215,8 +161,6 @@ fetch("news.json")
 
 
 
-
-
       }
 
 
@@ -226,40 +170,6 @@ fetch("news.json")
 
     /* **********************FILTRERING*****************************************/
     displayList(data);
-
-
-
-    // // Om vi vill filtrera på andra saker -title, date 
-    // const dates = list.getElementsByClassName('item-date');
-    // const titles = list.getElementsByClassName('item-name')
-    // const all = list.getElementsByClassName('items'); 
-
-    // const searchBar = forms['search-box'].querySelector('input');
-    // searchBar.addEventListener('keyup', (e) => {
-    //   const term = e.target.value.toLowerCase();
-    //   const types = list.getElementsByClassName('item-meta');
-
-    //   Array.from(types).forEach((typ) => {    //Array.from('foo') ger Array ["f", "o", "o"]
-    //     const type = typ.textContent;
-    //     if(type.toLowerCase().indexOf(e.target.value) != -1){  //här kollar man om detta som user skrev är "= article" eller "=news"
-    //       typ.style.display = 'none';
-    //       Array.from(titles).forEach((tit) => {
-    //         tit.style.display = 'block';
-    //       })
-    //       Array.from(dates).forEach((d) => {
-    //         d.style.display = 'block';
-    //       })
-    //     } else {
-    //       typ.style.display = 'none';
-    //     typ.parentElement.style.display = 'none';
-
-    //     }
-    //   });
-    // });
-
-
-
-
 
 
   })
